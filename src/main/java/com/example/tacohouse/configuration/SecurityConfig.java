56ff6d6d58767b0fe254bb66ce7608c3f80356bc
Ansh Anand <ansh.anand100@gmail.com>
2023-09-13 04:22:43 +0530

    initial commit

--- src/main/java/com/example/tacohouse/configuration/SecurityConfig.java
+++ src/main/java/com/example/tacohouse/configuration/SecurityConfig.java
@@ -2,22 +2,18 @@ package com.example.tacohouse.configuration;
 
 import com.example.tacohouse.model.User;
 import com.example.tacohouse.repositories.UserRepository;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
-
-import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
-
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -36,13 +32,14 @@ public class SecurityConfig {
         };
     }
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception{
-        MvcRequestMatcher.Builder mvc = new MvcRequestMatcher.Builder(introspector);
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
                 .authorizeHttpRequests((auth)-> auth
-                        .requestMatchers(mvc.pattern("/design") , mvc.pattern("/orders"),mvc.pattern("/orders/current"))
+                        .requestMatchers(("/design"), ("/orders"), ("/orders/current"))
                         .hasAnyRole("USER","ADMIN")
-                        .requestMatchers(mvc.pattern("/"), mvc.pattern("/**")).permitAll())
+                        .requestMatchers(("/"),("/**")).permitAll()
+                        .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ADMIN")
+                        .requestMatchers(toH2Console()).permitAll())
                 .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .formLogin(form->
                         form.loginPage("/login")
@@ -50,9 +47,5 @@ public class SecurityConfig {
                 .logout(logout->
                         logout.logoutSuccessUrl("/login"));
         return http.build();
-    }
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers(antMatcher("/h2-console/**"));
     }
 }
