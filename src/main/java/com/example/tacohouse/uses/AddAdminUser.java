2f2a24572fdecd2e4b0fac61225d0dbae05b8773
Ansh Anand <ansh.anand100@gmail.com>
2023-09-14 03:07:27 +0530

    initial commit

--- src/main/java/com/example/tacohouse/uses/AddAdminUser.java
+++ src/main/java/com/example/tacohouse/uses/AddAdminUser.java
@@ -2,14 +2,20 @@ package com.example.tacohouse.uses;
 
 import com.example.tacohouse.model.User;
 import com.example.tacohouse.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class AddAdminUser implements CommandLineRunner {
     private UserRepository userRepository;
+    @Value("${admin.password}")
+    private String password;
     private PasswordEncoder passwordEncoder;
+
     public AddAdminUser(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
@@ -17,7 +23,7 @@ public class AddAdminUser implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         if(userRepository.findByUsername("admin")==null){
-            userRepository.save(new User("admin",passwordEncoder.encode("12345"),"ROLE_ADMIN"));
+            userRepository.save(new User("admin",passwordEncoder.encode(password),"ROLE_ADMIN"));
         }
     }
 }
