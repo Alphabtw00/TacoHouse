9bfe6631ae08c3c79fefe1f9d36ec375e61f04d8
alpha <ansh.anand100@gmail.com>
2024-09-30 02:25:28 +0530

    .

--- src/main/java/com/example/tacohouse/entities/Taco.java
+++ src/main/java/com/example/tacohouse/entities/Taco.java
@@ -25,17 +25,17 @@ public class Taco{
     @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
 
-    @ManyToMany
-    @JoinTable(
+    @ManyToMany                                                                  //Taco is owning side as it manages the relation
+    @JoinTable(                                                                 //to name the extra table made by list elements
             name = "ingredient_Taco_Ref",
-            joinColumns = @JoinColumn(name = "taco_id"),
-            inverseJoinColumns = @JoinColumn(name = "ingredient_id")) //to name the extra table made by list elements
+            joinColumns = @JoinColumn(name = "taco_id"),                       //joinColumns expects the column to be of the owning side, @JoinColumn add that column, by default it adds the primary key column of the mapped entity(owning in this case)
+            inverseJoinColumns = @JoinColumn(name = "ingredient_id"))
     @NotNull
     @Size(min=1, message="You must choose at least 1 ingredient")
     private List<Ingredient> ingredients = new ArrayList<>();
 
     @ManyToOne
-    @JoinColumn(name = "taco_order_id")
+    @JoinColumn(name = "taco_order_id") //@joinColumn is used to specify foreign key, name is to set the name of the extra column made (by default it looks for the primary key of the the collection it references(mappedBy), to change use referredColumnName= and it will map that column instead
     @ToString.Exclude // to remove circular dependency when we log it
     private TacoOrder tacoOrder;
 
